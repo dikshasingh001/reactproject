502c487350062a7689a439e5adcdb41cd9368363
dikshasingh0102 <dikshasingh0102@gmail.com>
2023-09-06 16:50:58 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Login</h1>
+       <input type='text' placeholder='User id'></input>
+       <input type='text' placeholder='Password'></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
